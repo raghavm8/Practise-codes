f3489f06143fe3285e04e1eb1c95652ea5dfc7b9
raghavm8 <raghav.mittal1109@gmail.com>
2025-01-01 14:28:32 +0530

    codes added

--- Sorting/ElementsRemoval.java
+++ Sorting/ElementsRemoval.java
@@ -1,6 +1,26 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class ElementsRemoval {
     public int solve(int[] A) {
+        int n = A.length;
+        int ans = 0;
+        Arrays.sort(A);
+
+        for (int i = 0; i < n; i++) {
+            int num = A[n - i - 1];
+            ans = ans + (i + 1);
+        }
+
+        return ans;
     }
 }
+
+// 4 5 6 7 8 9
+// 9 8 7 6 5 4
+// 8 7 6 5 4
+// 7 6 5 4
+// 6 5 4
+// 5 4
+// 4
